b0a9a4d65ed8d5e65aad421fe7fb6be215ea846a
Plamen Yordanov <pyordanov@paggas.com>
2020-09-15 00:00:43 +0300

    statistics api- project structure

---
+++ apis/AgileTracker.StatisticsService/AgileTracker.StatisticsService.Web/WebConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AgileTracker.StatisticsService.Web
+{
+    public static class WebConfiguration
+    {
+        public static IServiceCollection AddWebComponents(this IServiceCollection services)
+        {
+            return services;
+                //.AddControllers();
+        }
+    }
+}
---
+++ apis/AgileTracker.StatisticsService/infrastructure/AgileTracker.StatisticsService.Infrastructure/InfrastructureConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace AgileTracker.StatisticsService.Infrastructure
+{
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class InfrastructureConfiguration
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
