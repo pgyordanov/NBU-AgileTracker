3eb6ecc24e5c9ad023f69a5efb05bd788eb65c7a
Plamen Yordanov <pyordanov@paggas.com>
2020-09-14 18:16:07 +0300

    statistics service- project structure update

---
+++ apis/AgileTracker.StatisticsService/infrastructure/AgileTracker.StatisticsService.Infrastructure/Persistance/AgileTrackerStatisticsDbContext.cs
@@ -0,0 +1,21 @@
+﻿namespace AgileTracker.StatisticsService.Infrastructure.Persistance
+{
+    using System.Reflection;
+
+    using Microsoft.EntityFrameworkCore;
+
+    public class AgileTrackerStatisticsDbContext: DbContext
+    {
+        public AgileTrackerStatisticsDbContext(DbContextOptions<AgileTrackerStatisticsDbContext> options)
+            :base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
