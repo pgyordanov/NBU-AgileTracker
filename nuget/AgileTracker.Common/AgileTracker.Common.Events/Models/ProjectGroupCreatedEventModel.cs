031ed7662bf59815d3a223cd51bb136b7092c079
Plamen Yordanov <pyordanov@paggas.com>
2020-09-17 13:39:41 +0300

    nuget packages- common events package

---
+++ nuget/AgileTracker.Common/AgileTracker.Common.Events/Models/ProjectGroupCreatedEventModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AgileTracker.Common.Events.Models
+{
+    public class ProjectGroupCreatedEventModel
+    {
+        public int ProjectGroupId { get; set; }
+
+        public string ProjectGroupOwnerId { get; set; } = default!;
+
+        public string ProjectGroupName { get; set; } = default!;
+    }
+}
---
+++ nuget/AgileTracker.Common/AgileTracker.Common.Events/Models/TaskFinishedEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace AgileTracker.Common.Events.Models
+{
+    using System;
+
+    public class TaskFinishedEvent
+    {
+        public int ProjectGroupId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int TaskId { get; set; }
+
+        public DateTime TaskFinishedOn { get; set; }
+    }
+}
